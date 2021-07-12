1ced8c5fa044849fa856bd2efbd0f2ce0026d38b
thoggs <thiagothogs@icloud.com>
2021-07-12 03:37:39 -0300

    add structure for menu

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -7,7 +7,7 @@ namespace EditorHtml
         public static void Show()
         {
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             DrawScreen();
         }
 
@@ -16,12 +16,41 @@ namespace EditorHtml
             UpperLine();
             CenterLine();
             DrawLine();
+            WriteOptions();
+        }
+
+        private static int WriteOptions()
+        {
+            Console.SetCursorPosition(10, 1);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(5, 3);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("1 => Novo arquivo");
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("2 => Abrir");
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("0 => Sair");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("Opção:");
+            Console.SetCursorPosition(9, 9);
+            try
+            {
+                int option = int.Parse(Console.ReadLine()!);
+                return option;
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                DrawScreen();
+                throw;
+            }
         }
 
         private static void UpperLine()
         {
             Console.Write("+");
-            for (int i = 0; i <= 30; i++)
+            for (int i = 0; i <= 50; i++)
             {
                 Console.Write("-");
             }
@@ -31,10 +60,10 @@ namespace EditorHtml
 
         private static void CenterLine()
         {
-            for (int lines = 0; lines <= 10; lines++)
+            for (int lines = 0; lines <= 15; lines++)
             {
                 Console.Write("|");
-                for (int i = 0; i <= 30; i++)
+                for (int i = 0; i <= 50; i++)
                 {
                     Console.Write(" ");
                 }
@@ -46,7 +75,7 @@ namespace EditorHtml
         private static void DrawLine()
         {
             Console.Write("+");
-            for (int i = 0; i <= 30; i++)
+            for (int i = 0; i <= 50; i++)
             {
                 Console.Write("-");
             }
