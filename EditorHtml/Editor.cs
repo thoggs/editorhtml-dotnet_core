1126fdf9aa47d6d3cfd7c43a5d1accaa3b4f09de
thoggs <thiagothogs@icloud.com>
2021-07-13 04:55:40 -0300

    add editor option

--- EditorHtml/Editor.cs
+++ EditorHtml/Editor.cs
@@ -1,7 +1,36 @@
+using System;
+using System.Text;
+using static System.ConsoleKey;
+
 namespace EditorHtml
 {
-    public class Editor
+    public static class Editor
     {
-        
+        public static void Show()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.SetCursorPosition(6, 1);
+            Console.WriteLine("MODO EDITOR");
+            Console.SetCursorPosition(4, 2);
+            Console.WriteLine("---------------");
+            Start();
+        }
+
+        private static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while (Console.ReadKey().Key != Escape);
+            
+            Console.WriteLine("---------------");
+            Console.WriteLine("Deseja salvar o arquivo?");
+            
+        }
     }
 }
