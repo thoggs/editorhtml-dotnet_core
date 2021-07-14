a2e1852669395973e00692211bd1f5c717c0de4e
thoggs <thiagothogs@icloud.com>
2021-07-14 04:49:27 -0300

    implemented Viewer.cs

--- EditorHtml/Editor.cs
+++ EditorHtml/Editor.cs
@@ -30,7 +30,7 @@ namespace EditorHtml
             
             Console.WriteLine("---------------");
             Console.WriteLine("Deseja salvar o arquivo?");
-            
+            Viewer.Show(file.ToString());
         }
     }
 }
